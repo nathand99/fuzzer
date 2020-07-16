77788acf4592c3e056e0003e92006d46f3806f2e
Rex <Rex@Macbook-Pro.local>
2020-07-16 19:22:22 +1000

    Added basic numeric fuzzing to jsonFuzzer

--- modules/jsonFuzzer.py
+++ modules/jsonFuzzer.py
@@ -1,10 +1,14 @@
+import json
+import copy
+import numbers
+from .fuzzerClass import fuzzerClass
 
-#Refer to csv fuzzer for idea of whats supposed to happen
-def makePayload():
-    #TODO
+# Convert python object into json string
+def makePayload(data):
+    return json.dumps(data)
     return
 
-class jsonFuzzer:
+class jsonFuzzer(fuzzerClass):
 
     def __init__(self, binary, data):
         self.binary = binary
@@ -14,6 +18,31 @@ class jsonFuzzer:
     #Main class function which runs all the different techniques
     def fuzz(self):
         #TODO
+        self.fuzzNumeric()
         return #remove after implementation
 
+
+
     #Other Techniques
+    
+    # Mess with numeric fields
+    def fuzzNumeric(self):
+        d=copy.deepcopy(self.data)  
+       
+        numericFields = []
+
+        for key in d.keys():
+            print(d[key])
+            if isinstance(d[key], numbers.Number):
+                numericFields.append(key)
+
+        for field in numericFields:
+            d[field] = 999999
+            self.usePayload(d)
+            d[field] = 0
+            self.usePayload(d)
+            d[field] = -1
+            self.usePayload(d)
+
+        #self.usePayload(d)
+
