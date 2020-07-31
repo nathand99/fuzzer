9a11396a5f0d8964221cddf3fae922bd27480fe8
Rex <rex.xue.lin@gmail.com>
2020-07-31 19:52:03 +1000

    added method that trys each subset of the json object

--- modules/jsonFuzzer.py
+++ modules/jsonFuzzer.py
@@ -1,6 +1,7 @@
 import json
 import copy
 import numbers
+import itertools
 from .fuzzerClass import fuzzerClass
 
 # Convert python object into json string
@@ -25,15 +26,41 @@ class jsonFuzzer(fuzzerClass):
             if isinstance(d[key], numbers.Number):
                 numericFields.append(key)
 
+
+        
         for field in numericFields:
-            d[field] = 999999
+
+            d[field] = 0x7FFFFFFF
             self.usePayload(d)
             d[field] = 0
             self.usePayload(d)
             d[field] = -1
             self.usePayload(d)
-
+            d[field] = 0xFFFFFFFF
+            self.usePayload(d)
+        
         #self.usePayload(d)
+        # Do every combination of test values on every subset of numeric fields
+        # Will take too long if there are too many numeric fields.
+        # Fix this later (OSError: [Errno 24] Too many open files)
+
+        testValues = [0, -1, 0x7FFFFFFF, 0xFFFFFFFF]
+        n = len(testValues)
+        i = 0
+
+        limit = n**len(numericFields)
+        if (limit > 100):
+            limit = 100
+
+        while i < (limit):
+            d = copy.deepcopy(self.data)  
+
+            for i2 in range(0, len(numericFields)):
+                d[numericFields[i2]] = testValues[ int(i /(n**i2))  %n]
+            self.usePayload(d)
+            #print(d)
+            i += 1
+
 
     # fuzz strings
     def fuzzStrings(self):
@@ -59,6 +86,8 @@ class jsonFuzzer(fuzzerClass):
             self.usePayload(d)
             d[field] = None
             self.usePayload(d)
+            d[field] = "%n" * 100
+            self.usePayload(d)
     
     # fuzz floats
     def fuzzFloat(self):
@@ -83,17 +112,13 @@ class jsonFuzzer(fuzzerClass):
             d[field] = 0.000000001
             self.usePayload(d)
 
-    # default input
-    def fuzzDefault(self):
-        print("===>Trying default input")
-        ret = self.data
-        self.usePayload(ret)
+        
 
+    
     # fuzz list
     def fuzzList(self):
         print("===>Trying fuzz lists")
         d = copy.deepcopy(self.data)  
-        self.usePayload(d)
         listFields = []
 
         for key in d.keys():
@@ -101,6 +126,8 @@ class jsonFuzzer(fuzzerClass):
                 listFields.append(key)
         #default length is 12 - give 13
         for field in listFields:
+            d[field] = d[field].append["A"]
+            self.usePayload(d)
             d[field] = []
             self.usePayload(d)
             d[field] = ["a"]
@@ -114,4 +141,27 @@ class jsonFuzzer(fuzzerClass):
             d[field] = ["A"]*100
             self.usePayload(d)
     
+    # default input
+    def fuzzDefault(self):
+        print("===>Trying default input")
+        ret = self.data
+        self.usePayload(ret)
+
+
+    # Try send each possible subset of fields
+    def fuzzSubsets(self):
+
+        l = self.data.keys()
+        for i in range(0, len(l) + 1):  
+            for subset in itertools.combinations(l, i):
+                d = {}
+                for item in subset:
+                    d[item] = self.data[item]
+                self.usePayload(d)
+                #print(d)
+
+
+    # Add additonal fields?
+    # Send format strings?
+    #
 
